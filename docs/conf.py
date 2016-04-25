e59633c73793f012fd49074d648fa4845abd86f9
Gregor Sturm <mail@gregor-sturm.de>
2016-04-25 12:15:56 +0200

    rename README to index

--- docs/conf.py
+++ docs/conf.py
@@ -49,7 +49,7 @@ source_suffix = ['.rst', '.md']
 #source_encoding = 'utf-8-sig'
 
 # The master toctree document.
-master_doc = 'README'
+master_doc = 'index'
 
 # General information about the project.
 project = 'microbio'
